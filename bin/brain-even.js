b18a0e298b03e04d03909c1c9381febcca453695
Дарья Фирсова <dfirsova648@gmail.com>
2022-12-13 12:53:07 +0300

    update bin/brain-even.js bin/brain-prime.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,8 +3,7 @@ import questionAnswer from '../src/cli.js'
 import { randomNumbers, evenOdd, conclusion, counter } from '../src/index.js'
 const parityCheck = () => {
     const name = questionAnswer();
-    const text = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-    console.log(text);
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
     for (let i = 0; i < 3; i++) {
         const randomNumber = randomNumbers(1, 30);
         console.log(`Question: ${randomNumber}`);
